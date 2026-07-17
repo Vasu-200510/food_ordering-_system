14263950820a1c754bce26122490709974213255
Vasu <vasu64303@gmail.com>
2026-07-17 11:12:59 +0530

    UPDATE

--- app.py
+++ app.py
@@ -202,6 +202,6 @@ def contact():
     return render_template("contact.html")
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
